cdd294b44e803cb902457f005f82ed98d5728c63
Sirius <siriusrbxdc@gmail.com>
2025-10-21 12:11:22 -0300

    Change JFrame title from 'Number RPG' to 'Counting Example'

--- example/Main.java
+++ example/Main.java
@@ -7,7 +7,7 @@ public class Main {
 	public static void main(String[] args) {
 		
 		JFrame frame = new JFrame();
-		frame.setTitle("Number RPG");
+		frame.setTitle("Counting Example");
 		frame.setSize(800, 600);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setResizable(true);
